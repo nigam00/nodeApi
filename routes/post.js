72494cf617931d2c3914db7e93831ea300fe3047
Nigam Agarwala <nigam.agarwal@abyeti.com>
2020-01-19 17:50:29 +0530

    Posts- create view delete update User - update delete view

--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,21 @@
 const express = require('express')
-const {getPosts, createPost} = require('../controllers/post')
+const {getPosts, createPost, postedByUser, postById, isPoster, deletePost, updatePost} = require('../controllers/post')
 const {requireSignin} = require('../controllers/auth')
 const {userById} = require('../controllers/user')
 const {createPostValidator} = require('../validator')
 
 const router = express.Router()
 
-router.get("/", getPosts)
-router.post("/post",requireSignin, createPostValidator,createPost)
+router.get("/posts", getPosts)
+router.post("/post/new/:userId",requireSignin,createPost, createPostValidator)
+router.get("/posts/by/:userId", requireSignin, postedByUser)
+router.delete('/post/:postId', requireSignin, isPoster, deletePost)
+router.put('/post/:postId', requireSignin, isPoster, updatePost)
+
 
 //any routes containing userid , our app will first execute userbyId method
 router.param("userId", userById)
+//any routes containing postid , our app will first execute postById method
+router.param("postId", postById)
 
 module.exports = router;
